e7ce79be3ee981b33950eb4548014e4498570f1a
Kweku <kwekuappiah11@gmail.com>
2023-01-25 07:39:25 +0000

    added defines file; instagram account id

--- api/facebook/facebook_manager.php
+++ api/facebook/facebook_manager.php
@@ -1,6 +1,8 @@
 <?php
 	require_once(__DIR__."/vendor/autoload.php");
 	require_once(__DIR__."/../utils/env_manager.php");
+	require_once(__DIR__."/../utils/define.php");
+	require_once(__DIR__."/../utils/http_handler.php");
 
 	//facebook array
 	$creds = array(
@@ -42,10 +44,32 @@
 
 		}
 
-		return $null;
+		return null;
 	}
 
 
+	//returns the instagram account id for the user who has completed the facebook
+	// oauth process
+	function get_instagram_account_id($accessToken){
+		//to access the instagram account id of a user, we use their oauth access token to obtain
+		//their facebook account info (important attribute being page id). Using the page
+		//id, we can call another endpoint to obtain the associated instagram account id
+		$endpoint = FACEBOOK_ENDPOINT_BASE. "me/accounts?access_token=$accessToken";
+
+		$http = new http_handler();
+		$response = $http->get($endpoint);
+		$response = json_decode($response,true)["data"];
+
+		$page_id = $response[0]["id"];
+
+
+		$insta_endpoint = FACEBOOK_ENDPOINT_BASE. "$page_id?fields=instagram_business_account&access_token=$accessToken";
+
+		$insta_response =  $http->get($insta_endpoint);
+		$insta_response = json_decode($insta_response,true);
+		$instagram_id = $insta_response["instagram_business_account"]["id"];
+
+	}
 
 
 
