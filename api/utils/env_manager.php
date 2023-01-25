4bd874801bd166074ae2cc208ba895c9643cef3a
Kweku <kwekuappiah11@gmail.com>
2023-01-25 07:20:23 +0000

    changed session management

--- api/utils/env_manager.php
+++ api/utils/env_manager.php
@@ -1,6 +1,9 @@
 <?php
 
 require_once (__DIR__."/../vendor/autoload.php");
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 
 $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
